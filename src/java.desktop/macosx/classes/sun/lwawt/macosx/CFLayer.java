f7d3d0767fa7fec21d1f26847c5326c3f4ee5402
jayathirthrao <jayathirth.d.v@oracle.com>
2021-02-11 16:15:37 +0530

    Update copyright year

--- src/java.desktop/macosx/classes/sun/lwawt/macosx/CFLayer.java
+++ src/java.desktop/macosx/classes/sun/lwawt/macosx/CFLayer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,6 +28,9 @@ package sun.lwawt.macosx;
 import sun.java2d.SurfaceData;
 import sun.java2d.NullSurfaceData;
 
+/**
+ * Common layer class between OpenGl and Metal.
+ */
 public abstract class CFLayer extends CFRetainedResource {
     protected SurfaceData surfaceData; // represents intermediate buffer (texture)
 
